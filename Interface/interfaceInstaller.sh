ee8fedf407d92dd8916db815684f10becb30e4c8
Marien Wolthuis <marien@marienwolthuis.nl>
2015-01-29 11:58:58 +0100

    Fixing symlinks

--- Interface/interfaceInstaller.sh
+++ Interface/interfaceInstaller.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 g++ Interface.cpp `pkg-config --libs gtk+-2.0` `pkg-config --cflags gtk+-2.0` -o interface.out -I. -L.
 chmod a+x ./runInterface.sh
-ln -s runInterface.sh ~/Desktop/PAK_Prototype_Interface
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+ln -s DIR/runInterface.sh ~/Desktop/PAK_Prototype_Interface
--- Server/serverInstaller.sh
+++ Server/serverInstaller.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 g++ server.cpp -o server.out -I. -L.
 chmod a+x ./runServer.sh
-ln -s runServer.sh ~/Desktop/PAK_Prototype_Server
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+ln -s DIR/runServer.sh ~/Desktop/PAK_Prototype_Server

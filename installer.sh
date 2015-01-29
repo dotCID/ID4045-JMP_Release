0da4848145de00a616dc0b15364cfe6cfe63b7d8
Marien Wolthuis <marien@marienwolthuis.nl>
2015-01-29 11:25:08 +0100

    Added some scripts

--- installer.sh
+++ installer.sh
@@ -4,14 +4,14 @@ add-apt-repository universe
 apt-get update
 apt-get install ekiga g++ libgtk2.0-dev build-essential git pkg-config arduino arduino-core -y
 
-git clone https://github.com/dotcid/ID4045-JMP_Complete-Code
+git clone https://github.com/dotcid/ID4045-JMP_Release
 cd Complete-Code/Interface
 chmod a+x ./interfaceInstaller.sh
 ./interfaceInstaller.sh
 
 cd ../Server
-g++ server.cpp -o server.out -I. -L.
-ln -s server.out ~/Desktop/PAK_Prototype_Server
+chmod a+x ./serverInstaller.sh
+./serverInstaller.sh
 
 cd ../Arduino
 mkdir ~/sketchbook
